880ff333f36515c1695a2892bdaff5febb51f60a
Osita Igwe <43799140+oigwe@users.noreply.github.com>
2021-01-03 22:33:50 -0500

    Update script.py
    added the ability to order more than one drink

--- script.py
+++ script.py
@@ -1,35 +1,54 @@
-from utils import print_message, get_size, order_latte, hot_or_cold, get_drink_type
+from utils import print_message, get_size, order_latte, hot_or_cold, get_drink_type, order_drink
 
 # Main Function
 
 def coffee_bot():
 
+  # Greeting
   print('Welcome to the cafe!')
 
-  # Drink Size
-  size = get_size()
-  print("Size:", size, "\n")
-
-  # Drink Type
-  drink_type = get_drink_type()
-  print("Drink Type: ", drink_type, "\n")
-
-  # Drink Temperature
-  drink_temp = hot_or_cold()
-  print("Temperature: ", drink_temp, "\n")
-
-  # Order Confirmation Message Template
-  message = "\nAlright, that's a {} {} {}".format(size, drink_temp, drink_type)
-
-  
-  # Order Confirmation Message
-  print(message)
-
   # Customer Name
-  name = input('\nCan I get your name please? \n>')
+  name = input('My name is Cora. I am your virtual barista. What is your name?\n>')
 
-  # Order Status
-  return 'Thanks, {}! Your drink will be ready shortly.".format(name)
+  order = []
+
+  drink_order = order_drink()
+
+  while drink_order == 'Yes':
+
+    # Drink Size
+    size = get_size()
+    print("Size:", size, "\n")
+
+    # Drink Type
+    drink_type = get_drink_type()
+    print("Drink Type: ", drink_type, "\n")
+
+    # Drink Temperature
+    drink_temp = hot_or_cold()
+    print("Temperature: ", drink_temp, "\n")
+
+    # Order Confirmation Message Template
+    message = "\nAlright, that's a {} {} {}".format(size, drink_temp, drink_type)
+  
+    # Order Confirmation Message
+    #print(message)
+
+    order.append('{} {} {}'.format(size, drink_temp, drink_type))
+    
+    # Order Status
+    print('Your drink will be ready shortly.\n')
+
+    # New Drink Order ?
+    drink_order = order_drink()
+
+  if len(order) > 0:
+    print('\nOkay, so I have an order for:')
+    for drink in order:
+      print('a ' + drink )
+
+  return  '\nI hope to see you soon {}!'.format(name)
+
 
 # Call coffee_bot()!
 
